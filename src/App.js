e5beaef4ec9d84d6609468ef3b4dfd12797c10aa
bartosian <bartosian1989@gmail.com>
2020-08-16 21:36:25 -0400

    Added sveral function components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title: 'React'
+};
+
+function getTitle(title) {
+  return title;
+}
+
+const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3,
+      points: 4,
+      objectID: 0,
+      }, {
+      title: 'Redux',
+      url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const handleSearch = event => {
+    console.log(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Hello {getTitle('React')}
+      </h1>
+
+      <Search onSearch={handleSearch}/>
+      <List list={stories}/>
     </div>
   );
+}
+
+const Search = (props) => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+    props.onSearch(event);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <p>
+      Searching for <strong>{searchTerm}</strong>.
+      </p> 
+    </div>
+  );
+};
+
+const List = props => {
+  return props.list.map(item => <div key={item.objectID}>{item.title}</div>);
 }
 
 export default App;
